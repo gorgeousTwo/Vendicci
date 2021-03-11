357714abe2e91f56726ab9ca22d3b202e5fc4530
gorgeousTwo <gitmiu@gmail.com>
2021-03-11 17:26:40 +0900

    create dto first

---
+++ src/main/java/com/model/memberT.java
@@ -0,0 +1,67 @@
+package com.model;
+
+public class memberT {
+    private String id;
+    private String pwd;
+    private String phone;
+    private String email;
+    private String bio;
+    private String country;
+    private String address;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
